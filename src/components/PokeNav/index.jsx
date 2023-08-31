e48a99c072088acc488904f4d5d044b243731dab
Jorge Vinicius <jorgeviniciuslucas@hotmail.com>
2023-08-31 17:28:13 -0300

    ~

--- src/components/PokeNav/index.jsx
+++ src/components/PokeNav/index.jsx
@@ -8,7 +8,7 @@ export const PokeNav = ({tipo, nome, id}) => {
     <nav className={`border-gray-200 bg-${tipo} text-green-300`}>
         <div className="flex flex-wrap items-center justify-between mx-auto p-4">
             <Link to='/' className="flex items-center gap-x-4">
-                <AiOutlineArrowLeft className="w-6 h-6 text-white"/>
+                <AiOutlineArrowLeft className="w-8 h-8 text-white"/>
             </Link>
             <h2 className="self-center text-2xl font-semibold whitespace-nowrap capitalize text-white">{nome}</h2>
             <p className="text-base text-white">#{pokemonId(id)}</p>
